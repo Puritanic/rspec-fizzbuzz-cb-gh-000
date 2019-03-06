0e61adfb757a091698c931f6089220f3d17dad2d
Darko Tasevski <darktasevski@gmail.com>
2019-03-06 10:50:43 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+  if int % 3 == 0 # if the number int is divisible by 3
+    puts "Fizz" # Go fizz
+  elsif int % 15 == 0
+    puts "FizzBuzz" # Go fizz
+  elsif int % 5 == 0
+    puts "Buzz"
+end
